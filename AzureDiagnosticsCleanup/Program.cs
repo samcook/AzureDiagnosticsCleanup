b9b8b690b99e1737c4146317ca80ea18017af756
Sam Cook <github@sam.net.nz>
2024-04-11 18:19:44 +1200

    Added additional modes

--- AzureDiagnosticsCleanup/Program.cs
+++ AzureDiagnosticsCleanup/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Globalization;
 using System.Net;
 using System.Threading.Tasks.Dataflow;
@@ -18,12 +17,18 @@ namespace AzureDiagnosticsCleanup
 
             Console.CancelKeyPress += (sender, e) =>
             {
-                Console.WriteLine("Canceling...");
+                Console.WriteLine("Cancelling...");
                 cts.Cancel();
                 e.Cancel = true;
             };
 
-            var configBuilder = new ConfigurationBuilder().AddJsonFile("config.json");
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddCommandLine(args, new Dictionary<string, string>
+                {
+                    {"-m", "Mode"}
+                });
+
             var config = configBuilder.Build();
 
             ServicePointManager.DefaultConnectionLimit = Math.Max(2, Convert.ToInt32(config["MaxDOP"] ?? "1"));
@@ -31,15 +36,53 @@ namespace AzureDiagnosticsCleanup
             var adc = new AzureDiagnosticsCleanup(
                 new TableServiceClient(
                     new Uri(config["AzureTableStorage:Url"] ?? throw new ApplicationException("Table storage URL not specified")),
-                    new AzureSasCredential(config["AzureTableStorage:SASToken"] ?? throw new ApplicationException("SAS Token not specified"))));
+                    new AzureSasCredential(config["AzureTableStorage:SASToken"] ?? throw new ApplicationException("SAS Token not specified"))
+                    //new TableClientOptions
+                    //{
+                    //    Retry = { NetworkTimeout = TimeSpan.FromSeconds(120) }
+                    //}
+                ));
 
-            await adc.CleanupAzureDiagnosticsDataAsync(
-                    config["AzureTableStorage:TableName"] ?? throw new ApplicationException("Table name not specified"),
-                    DateTimeOffset.Parse(config["CutOffDate"] ?? throw new ApplicationException("Cutoff Date not specified"), DateTimeFormatInfo.InvariantInfo),
-                    Convert.ToInt32(config["MaxDOP"] ?? "1"),
-                    Convert.ToInt32(config["StatusIntervalSeconds"] ?? "1"),
-                    cts.Token)
-                .ConfigureAwait(false);
+            switch (config["Mode"])
+            {
+                case "WADMetrics":
+                    await adc.CleanupWADMetricsTablesAsync(
+                            "WADMetrics",
+                            s => (s[..21], new DateTimeOffset(int.Parse(s[21..25]), int.Parse(s[25..27]), int.Parse(s[27..29]), 0, 0, 0, TimeSpan.Zero)),
+                            DateTimeOffset.Parse(config["CutOffDate"] ?? throw new ApplicationException("Cutoff Date not specified"), DateTimeFormatInfo.InvariantInfo),
+                            bool.Parse(config["DryRun"] ?? "true"),
+                            cts.Token)
+                        .ConfigureAwait(false);
+                    break;
+
+                case "WADData":
+                    await adc.CleanupWADDataAsync(
+                            config["Data:TableName"] ?? throw new ApplicationException("Table name not specified"),
+                            DateTimeOffset.Parse(config["CutOffDate"] ?? throw new ApplicationException("Cutoff Date not specified"), DateTimeFormatInfo.InvariantInfo),
+                            Convert.ToInt32(config["MaxDOP"] ?? "1"),
+                            Convert.ToInt32(config["StatusIntervalSeconds"] ?? "1"),
+                            bool.Parse(config["DryRun"] ?? "true"),
+                            cts.Token)
+                        .ConfigureAwait(false);
+
+                    break;
+
+                case "LinuxData":
+                    await adc.CleanupLinuxDataAsync(
+                            config["Data:TableName"] ?? throw new ApplicationException("Table name not specified"),
+                            DateTimeOffset.Parse(config["CutOffDate"] ?? throw new ApplicationException("Cutoff Date not specified"), DateTimeFormatInfo.InvariantInfo),
+                            Convert.ToInt32(config["MaxDOP"] ?? "1"),
+                            Convert.ToInt32(config["StatusIntervalSeconds"] ?? "1"),
+                            bool.Parse(config["DryRun"] ?? "true"),
+                            cts.Token)
+                        .ConfigureAwait(false);
+
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown mode");
+                    return;
+            }
         }
     }
 
@@ -55,15 +98,112 @@ namespace AzureDiagnosticsCleanup
             this.tableServiceClient = tableServiceClient;
         }
 
-        private static async Task ProcessBatch(TableClient tableClient, string? batchId, ICollection<TableTransactionAction> toDelete)
+        public async Task CleanupWADMetricsTablesAsync(string tablePrefix, Func<string, (string Prefix, DateTimeOffset IntervalStart)> tableNameParser, DateTimeOffset cutoff, bool dryRun, CancellationToken token = default)
         {
-            //Console.WriteLine($"Enter ProcessBatch({batchId}) with {toDelete.Count} items");
-            await tableClient.SubmitTransactionAsync(toDelete, CancellationToken.None).ConfigureAwait(false);
-            //Console.WriteLine($"Leave ProcessBatch({batchId})");
+            Console.WriteLine($"{nameof(CleanupWADMetricsTablesAsync)}({tablePrefix}, {cutoff:u})");
+
+            var rangeStart = tablePrefix;
+            var rangeEnd = tablePrefix[..^1] + (char)(tablePrefix[^1] + 1); // a bit hacky, but probably fine for this purpose
+            var filter = $"TableName ge '{rangeStart}' and TableName lt '{rangeEnd}'";
+
+            Console.WriteLine(filter);
+
+            string? previousName = null;
+
+            await foreach (var table in tableServiceClient.QueryAsync(filter).OrderBy(x => x.Name)
+                               .WithCancellation(CancellationToken.None)
+                               .ConfigureAwait(false))
+            {
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                // WADMetricsPT1HP10DV2S<yyyymmdd> - date is creation date of the table
+                // WADMetricsPT1MP10DV2S<yyyymmdd>
+                // AzureWebJobsHostLogs<yyyymm> (seem small)
+                var (previousNamePrefix, previousIntervalStart) = previousName != null
+                    ? tableNameParser(previousName)
+                    : ((string?)null, (DateTimeOffset?)null);
+                var (currentNamePrefix, currentIntervalStart) = tableNameParser(table.Name);
+
+                // if the prefix has changed, ignore the previous details
+                if (previousNamePrefix != currentNamePrefix)
+                {
+                    previousName = null;
+                    previousNamePrefix = null;
+                    previousIntervalStart = null;
+                }
+                
+                Console.WriteLine($"{previousName} => {table.Name}, {previousIntervalStart} => {currentIntervalStart}, {previousNamePrefix}/{currentNamePrefix}");
+
+                if (previousName != null && previousIntervalStart != null && currentIntervalStart < cutoff)
+                {
+                    if (!dryRun)
+                    {
+                        await tableServiceClient.DeleteTableAsync(previousName, CancellationToken.None).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Delete {previousName}");
+                    }
+                }
+
+                previousName = table.Name;
+            }
         }
 
-        public async Task CleanupAzureDiagnosticsDataAsync(string table, DateTimeOffset cutoff, int maxDop, int statusIntervalSeconds, CancellationToken token = default)
+        public async Task CleanupLinuxDataAsync(string table, DateTimeOffset cutoff, int maxDop, int statusIntervalSeconds, bool dryRun, CancellationToken token = default)
         {
+            Console.WriteLine($"{nameof(CleanupLinuxDataAsync)}({table}, {cutoff:u})");
+
+            for (var i = 0; i <= 9; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                var fromPartitionKey = $"{i:D19}___{0:D19}";                // 0000000000000000001___0000000000000000000
+                var toPartitionKey = $"{i:D19}___{cutoff.UtcTicks:D19}";    // 0000000000000000001___0636951868800000000
+
+                var filter = $"PartitionKey ge '{fromPartitionKey}' and PartitionKey lt '{toPartitionKey}'";
+
+                await CleanupTableDataAsync(
+                        table,
+                        filter,
+                        cutoff,
+                        maxDop,
+                        statusIntervalSeconds,
+                        x => long.TryParse(x?[22..], out var lastPartitionKeyLong) ? $"{new DateTimeOffset(lastPartitionKeyLong, TimeSpan.Zero):u}" : null,
+                        dryRun,
+                        token)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        public async Task CleanupWADDataAsync(string table, DateTimeOffset cutoff, int maxDop, int statusIntervalSeconds, bool dryRun, CancellationToken token = default)
+        {
+            Console.WriteLine($"{nameof(CleanupWADDataAsync)}({table}, {cutoff:u})");
+
+            var filter = $"PartitionKey le '{cutoff.UtcTicks:D19}'";
+
+            await CleanupTableDataAsync(
+                    table,
+                    filter,
+                    cutoff,
+                    maxDop,
+                    statusIntervalSeconds,
+                    x => long.TryParse(x, out var lastPartitionKeyLong) ? $"{new DateTimeOffset(lastPartitionKeyLong, TimeSpan.Zero):u}" : null,
+                    dryRun,
+                    token)
+                .ConfigureAwait(false);
+        }
+
+        private async Task CleanupTableDataAsync(string table, string filter, DateTimeOffset cutoff, int maxDop, int statusIntervalSeconds, Func<string?, string?> lastPartitionKeyStatusParser, bool dryRun, CancellationToken token = default)
+        {
+            Console.WriteLine($"Filter: {filter}");
+
             var sw = Stopwatch.StartNew();
 
             var toDelete = new List<TableTransactionAction>(100);
@@ -72,13 +212,11 @@ namespace AzureDiagnosticsCleanup
             var transactions = 0;
             string? lastPartitionKey = null;
 
-            Console.WriteLine($"{nameof(CleanupAzureDiagnosticsDataAsync)}({table}, {cutoff})");
-
             var tableClient = tableServiceClient.GetTableClient(table);
             var actionBlock = new ActionBlock<(string? BatchId, List<TableTransactionAction> ToDelete)>(
                 async x =>
                 {
-                    await ProcessBatch(tableClient, x.BatchId, x.ToDelete);
+                    await ProcessTableTransactionAsync(tableClient, x.BatchId, x.ToDelete, dryRun).ConfigureAwait(false);
                     Interlocked.Add(ref deleted, x.ToDelete.Count);
                     Interlocked.Increment(ref transactions);
                 },
@@ -90,57 +228,67 @@ namespace AzureDiagnosticsCleanup
 
             var timer = new Timer(state =>
             {
-                DateTimeOffset? lastQueuedDate = long.TryParse(lastPartitionKey, out var lastPartitionKeyLong) ? ConvertAzTimeToDateTime(lastPartitionKeyLong) : null;
-                // ReSharper disable once AccessToModifiedClosure
-                Console.WriteLine($"Last: {lastQueuedDate:u}, Queued: {queued}, Deleted: {deleted}, Txns: {transactions}, QD: {actionBlock.InputCount}, Time: {sw.Elapsed:g}, Del rate: {deleted/sw.Elapsed.TotalSeconds:F0}/sec");
+                // ReSharper disable AccessToModifiedClosure
+                Console.WriteLine($"Last: {lastPartitionKeyStatusParser(lastPartitionKey)}, Queued: {queued:N0}, Deleted: {deleted:N0}, Txns: {transactions:N0}, QD: {actionBlock.InputCount}, Time: {sw.Elapsed:g}, Del/s: {deleted/sw.Elapsed.TotalSeconds:F0}/s, Txn/s: {transactions/sw.Elapsed.TotalSeconds:F0}/s");
+                // ReSharper restore AccessToModifiedClosure
             }, null, TimeSpan.Zero, TimeSpan.FromSeconds(statusIntervalSeconds));
 
-            await foreach (var entity in tableClient.QueryAsync<TableEntity>(
-                                   filter: $"PartitionKey le '0{ConvertDateTimeToAzTime(cutoff)}'",
-                                   select: Columns,
-                                   cancellationToken: CancellationToken.None)
-                               .ConfigureAwait(false))
+            try
             {
-                //Console.WriteLine($"{ConvertAzTimeToDateTime(Convert.ToInt64(entity.PartitionKey))} {entity.PartitionKey} {entity.RowKey}");
+                await foreach (var entity in tableClient.QueryAsync<TableEntity>(
+                                       filter: filter,
+                                       select: Columns,
+                                       cancellationToken: CancellationToken.None)
+                                   .ConfigureAwait(false))
+                {
+                    //Console.WriteLine($"{ConvertAzTimeToDateTime(Convert.ToInt64(entity.PartitionKey))} {entity.PartitionKey} {entity.RowKey}");
 
-                //if (queued >= 10000)
-                //{
-                //    break;
-                //}
+                    //if (queued >= 10000)
+                    //{
+                    //    break;
+                    //}
 
-                if (toDelete.Count > 0 && (entity.PartitionKey != lastPartitionKey || toDelete.Count == 100))
+                    if (toDelete.Count > 0 && (entity.PartitionKey != lastPartitionKey || toDelete.Count == 100))
+                    {
+                        //Console.WriteLine($"Delete {lastPartitionKey}, items: {toDelete.Count}, qd: {actionBlock.InputCount}");
+
+                        var result = await actionBlock.SendAsync((lastPartitionKey, toDelete), CancellationToken.None).ConfigureAwait(false);
+                        if (!result)
+                        {
+                            throw new ApplicationException("Failed to send to action block");
+                        }
+
+                        toDelete = new List<TableTransactionAction>(100);
+                    }
+
+                    if (token.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    toDelete.Add(new TableTransactionAction(TableTransactionActionType.Delete, entity));
+
+                    lastPartitionKey = entity.PartitionKey;
+                    queued++;
+                }
+
+                if (toDelete.Count != 0)
                 {
                     //Console.WriteLine($"Delete {lastPartitionKey}, items: {toDelete.Count}, qd: {actionBlock.InputCount}");
 
                     var result = await actionBlock.SendAsync((lastPartitionKey, toDelete), CancellationToken.None).ConfigureAwait(false);
-                    if (!result) {
-                        Console.WriteLine("Failed to send to action block");
+                    if (!result)
+                    {
+                        throw new ApplicationException("Failed to send to action block");
                     }
-                    
-                    toDelete = new List<TableTransactionAction>(100);
                 }
-
-                if (token.IsCancellationRequested)
-                {
-                    break;
-                }
-
-                toDelete.Add(new TableTransactionAction(TableTransactionActionType.Delete, entity));
-
-                lastPartitionKey = entity.PartitionKey;
-                queued++;
             }
-
-            if (toDelete.Count != 0)
+            catch (ApplicationException ex) when (ex.Message == "Failed to send to action block")
             {
-                //Console.WriteLine($"Delete {lastPartitionKey}, items: {toDelete.Count}, qd: {actionBlock.InputCount}");
-
-                var result = await actionBlock.SendAsync((lastPartitionKey, toDelete), CancellationToken.None).ConfigureAwait(false);
-                if (!result)
-                {
-                    Console.WriteLine("Failed to send to action block");
-                }
+                Console.WriteLine("Failed to send to action block");
             }
+
+            Console.WriteLine("Completing action block");
 
             actionBlock.Complete();
             await actionBlock.Completion.ConfigureAwait(false);
@@ -151,25 +299,35 @@ namespace AzureDiagnosticsCleanup
             timer.Dispose(waitHandle);
             waitHandle.WaitOne();
 
-            Console.WriteLine($"Deleted {deleted} entries in {transactions} transactions in {sw.Elapsed:g}, Del rate: {deleted/sw.Elapsed.TotalSeconds:F0}/sec");
+            Console.WriteLine($"{table}: Deleted {deleted:N0} entries in {transactions:N0} transactions in {sw.Elapsed:g}, Del/s: {deleted/sw.Elapsed.TotalSeconds:F0}/s, Txn/s: {transactions / sw.Elapsed.TotalSeconds:F0}/s");
         }
 
-        public static long ConvertDateTimeToAzTime(DateTimeOffset dateTimeOffset)
+        private static async Task ProcessTableTransactionAsync(TableClient tableClient, string? batchId, ICollection<TableTransactionAction> toDelete, bool dryRun)
         {
-            var t = dateTimeOffset.AddYears(1600);
+            //Console.WriteLine($"Enter ProcessTableTransactionAsync({batchId}) with {toDelete.Count} items");
+            if (!dryRun)
+            {
+                var sw = Stopwatch.StartNew();
 
-            var az = (t.ToUnixTimeSeconds() + 11644473600) * (long)1e7;
+                try
+                {
+                    await tableClient.SubmitTransactionAsync(toDelete, CancellationToken.None).ConfigureAwait(false);
+                }
+                catch (TableTransactionFailedException ex)
+                {
+                    sw.Stop();
 
-            return az;
-        }
+                    Console.WriteLine($"SubmitTransactionAsync failed after {sw.ElapsedMilliseconds}ms: {ex}");
 
-        public static DateTimeOffset ConvertAzTimeToDateTime(long azTime)
-        {
-            var t = azTime / (long)1e7 - 11644473600;
-
-            var dt = DateTimeOffset.FromUnixTimeSeconds(t).AddYears(-1600);
-
-            return dt;
+                    // try one more time
+                    //await tableClient.SubmitTransactionAsync(toDelete, CancellationToken.None).ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                //Console.WriteLine($"Dry run: delete {batchId} with {toDelete.Count} items");
+            }
+            //Console.WriteLine($"Leave ProcessTableTransactionAsync({batchId})");
         }
     }
 }
